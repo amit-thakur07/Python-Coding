64543766157891e3a7bd9f260de5451a18a87c0d
Amit Thakur <amitthakur70072@gmail.com>
2026-03-04 11:37:07 +0530

    Add files via upload

---
+++ TUPPLES2 PRACTICE.py
@@ -0,0 +1,16 @@
+a=(1,2,34,4,5,5,6,)
+a.count(5)
+print(a)
+p =a.index(5)
+print(p)
+# a[0]=(23)
+# print(a)
+# print(type(a))
+
+# # a=(1,)
+# # print(a)
+
+# # print(type(a))
+
+#tupple methods
+# count()
---
+++ tupple.py
@@ -0,0 +1,17 @@
+# a=(1,2,3,"amit",2.23,2224.90)
+# print(type(a))
+# print(a)
+# b=("amit","aman","akshay")
+# print(type(b))
+
+# t=(10,)
+# print(t)
+# print(type(t))
+# a=10,20,30,4,5,6,7
+# print(type(a))
+
+
+# a1=1,2
+# a,b=a1
+# print(a)
+# print(b).......unpacking
