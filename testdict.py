cf6c46494805354fd5bc55d208d75e2f63fbd8d2
Amit Thakur <amitthakur70072@gmail.com>
2026-03-06 09:54:59 +0530

    Add files via upload

---
+++ testdict.py
@@ -0,0 +1,181 @@
+#  program 1:arithm op:sum of two numbers
+# a=12
+# b=10
+# print(a+b)
+
+#program 2:diffrence bw 2 num
+# a=20
+# b=10
+# print(a-b)
+
+#program 3:product
+# a=39
+# b=12
+# print(a*b)
+
+#program 4:divide 2 num
+# a=19
+# b=2
+# print(a/b)
+
+#pr:5 find the remainder
+# a=40
+# b=3
+# print(a%b)
+
+#pr:6 floor div op find quotient
+# a=29
+# b=3
+# print(a//b)
+
+# pr:7 sq and  cube of num
+# a=2
+# print(a*2)
+# print(a**3)
+
+# 8:avg of 3 num
+# a=3
+# b=4
+# c=6
+# print((a+b+c)/3)
+
+#9 compare 2 num
+# a=20
+# b=12
+# print(a==b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print(a<=18)
+
+#program:3 operators
+# a=1
+# b=2
+# c=4
+# print(a==b==c)
+
+#p: multiple comparision expression
+# a=2
+# b=1
+# print(a==b,a>b,a<b,b==a,b!=a)
+
+#logical:and op
+# a=12
+# b=13
+# print(a and b)
+
+#pr:.or op
+# a=10
+# b=12
+# print(a or b)
+
+#not op
+# a=12
+# print(not a)
+
+#num is +ve and less than 100
+# a=int(input("enter a numbr:"))
+# print(a>0 and a<100)
+
+#number is divisible by 2 or 5
+# a=int(input("enter a number: "))
+# print(a%2==0 or a%5==0)
+
+#a>b and b>c
+# a=12
+# b=10
+# c=11
+# print(a>b and b>c)
+
+# a=4......is operators
+# b=5
+# print(a is b)
+# print(a is not b)
+
+# a=2
+# b=2
+# print(a==b)
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# name="harry"
+# age= 20
+# print("my name is {} and age is {}".format(name,age))
+
+# letter='''dear <|name|>,
+# you are selected!
+# <|date|>'''
+# print(letter.replace("<|name|>","harry").replace("<|date|>","21 sept"))
+
+
+#detect a double space in a string
+# a="amit is a student of computer science and engineering"
+# print(a.find("com"))
+
+# a="dear amit, \tthis python programing is good ,\tThanks for choosing it !"
+# print(a)
+#this is the escape sequence character method use of \n and \t
+
+# find factorial
+
+# n=5
+# def fun(n):
+#   fact=1
+# for i in range(1,n+1):
+#     fact= fact*i
+      
+# x= fun(n)  
+# print(x)  
+# def print_full_name(first, last):
+#     return (f"Hello {first} {last}! You just delved into python.")
+# y=input()
+# z=input()
+# x=print_full_name(y,z)
+# print(x)
+
+# recursive function to print the sum of n natural number
+
+# sum1=0
+# for i in range(11):
+#     sum1= sum1+i
+# print(f"total sum of the numbers is {sum1}")
+    
+
+# Write a program to find the factorial of n = 5.
+
+# jet=1
+# for i in range(1,6):
+#     jet=jet*i
+# print(jet)
+
+# Print squares of numbers from 1 to 5
+
+# for i in range(1,6):
+#     print(i**2)
+
+# Print only the even numbers from 1 to 20
+# for i in range(1,21):
+#     if(i%2==0):
+#         print(i)
+
+# Print numbers from 1 to 10, but skip 5
+# for i in range(1,11):
+#     if(i==5):
+#         continue
+# print(i)
+
+# Count how many even numbers are there between 1 and 20
+# count=0
+# for i in range(1,21):
+#     if(i%2==0):
+#         count+=1
+# print(count)
+
+
+# PATTERN QUESTIONS 
+ 
+# for i in range(1,4):
+#     print("*"*n)
