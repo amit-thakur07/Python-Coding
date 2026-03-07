2d9ae4867fbee08bd44de3ed1f68349ee39361e5
Amit Thakur <amitthakur70072@gmail.com>
2026-03-07 09:59:18 +0530

    Add files via upload

---
+++ if_stmt.py
@@ -0,0 +1,48 @@
+# a=int(input("enter your number:"))
+# b=int(input("enter your number:"))
+# c=int(input("enter your number:"))
+# d=int(input("enter your number:"))
+# if(a>b and a>c and a>d):
+#     print("a is greater")
+# elif(b>a and b>c and b>d):
+#     print("b is greater")
+# elif(c>a and c>b and c>d):
+#     print("c is greater")
+# elif(d>a and d>b and d>c):
+#     print("d is greater")
+# else:
+#     print("thank you for your kindness")     .......else if program
+#
+# s1=int(input("enter your marks details sub1:"))
+# s2=int(input("enter your marks details sub2:"))
+# s3=int(input("enter your marks details sub3:"))
+# percentage=(s1+s2+s3)/3
+# if( percentage>=40 and s1>=33 and s2>=33 and s3>=33):
+#     print("you are passed!congracts",("percentage is: ",percentage))
+# else:
+#     print("failed in studies but not in life, so keep faith in yourself")    
+# p1="make a lot of money"
+# p2="buy now"
+# p3="subscribe now"
+# p4="click here"
+# mess=input("enter your message: ")
+# if(p1 in mess or p2 in mess or p3 in mess or p4 in mess):
+#     print("spam alert")
+# else:
+#     print("no spam")    
+
+# a= input("enter yr name:  ")
+# b=len(a)
+# if(b<=10):
+#     print("contains less than 10 characters")
+# else:
+#     print("contains more than 10 characters")    
+
+# a= int(input("enter your marks: "))
+# if(a>=90 or a==100):
+#     print("ex")
+a=input("enter name: ")
+if("HARRY" in a.upper()):
+    print("someone is talking about harry")
+else:
+    print("no one is talking about harry")    
