2a71eefb26c7ce4c79bd513e169b9d923a0d1af0
Amit Thakur <amitthakur70072@gmail.com>
2026-03-10 09:40:08 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,87 @@
+# i=10
+# while(i>=1):
+#      print(i)
+#      i-=1
+#printing all even numbers
+# i=1
+# while(i<=20):
+#     print(i)
+#     i+=2
+# i=0
+# while(i<=10):
+#     print("7 *",i,"=",7*i)
+#     i+=1...........................table of 7
+
+# printing the numbers untill 0 is provided: 
+# n=int(input("enter the number: "))
+# while(n!=0):
+#      print(n)
+#      n= int(input("enter a number: "))
+
+# print first n natural nuumbers
+# n=int(input("enter number: "))
+# for i in range(1,n+1):
+#     print(i)
+#sum of numbers from 1 to n
+# n=int(input("enter the number: "))
+# i=1
+# sum=0
+# while(i<=n):
+#     sum+=i
+#     i+=1
+# print("sum of 1 to",n,"is",sum)
+
+# # print the digits of a number
+# num = int(input("enter a number: "))
+# while num > 0:
+#   digit = num % 10    
+#   print(digit)
+#   num = num // 10 
+
+
+# count=1
+# while(count<=5):
+#     print("hello")
+#     count+=1
+# print(count)
+
+# count=1
+# while(count<=12):
+#     print("amit",count)
+#     count+=1
+
+# i=100
+# while(i>=1):
+#     print(i)
+#     i-=1
+# print("loop ended")
+
+# n=int(input("enter a number: "))
+# i=1
+# while(i<=10):
+#     print(n*i)
+#     i+=1
+
+# list=[1,4,9,16,25,36,49,64,81,100]
+# index=0
+# while(index<len(list)):
+#     print(list[index])
+#     index+=1
+    
+# tupple=1,2,3,4,5,6
+# i=0
+# while(i<len(tupple)):
+#     print(tupple[i])
+#     i+=1
+# print(i)
+
+
+
+
+
+
+
+    
+
+
+
