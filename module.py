e2086d44743034b7439b89ed81c2c68a8f82f4bd
Amit Thakur <amitthakur70072@gmail.com>
2026-03-11 10:01:15 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,32 @@
+import math
+# print(math.sqrt(36))
+# print(math.factorial(5))
+# print(math.pow(2,4))
+# a=4.3
+# print(math.ceil(a))
+# print(math.pi)
+# print(math.cos(0))
+# print(math.log10(10))
+ 
+
+
+
+# 2 RANDOM MODULE:
+# import random
+# print(random.randint(2,200))
+
+# print(random.random())
+# print(random.choice(["apple","amit","naman","kavi"]))
+# a=[1,2,3,4,5]
+# random.shuffle(a)
+# print(a)
+
+# 3 STASTISTIC MODULE:
+# import statistics
+# data=[10,2,30,300]
+# print(statistics.mean(data))
+# print(statistics.median(data))
+# print(statistics.mode(data))
+
+
+
