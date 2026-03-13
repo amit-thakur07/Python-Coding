58280227a6f33ce822d5ffdabd1b28e2f37e82db
Amit Thakur <amitthakur70072@gmail.com>
2026-03-13 11:03:46 +0530

    Add files via upload

---
+++ modules_practice.py
@@ -0,0 +1,37 @@
+import math
+import statistics
+
+a=[10,20,30,40,50]
+(statistics.mean(a))
+print(a)
+# print(math.sqrt(144))
+# print(math.pow(5,3))
+# print(math.factorial(7))
+# print(math.ceil(8.3))
+# print(math.floor(8.9))
+# print(math.pi)
+# print(math.sin(90))
+# print(math.gcd(48,18))
+# print(math.log10(1000))
+# a=-13
+# b=16
+# print(abs(a-b))/
+
+
+
+# # 2. RANDOM MODULE
+# import random
+# a=[1,23,2,2,2,2,2,2,2,2,2]
+# # print(random.randint(a))
+# print(random.randrange(1,20))
+# print(random.randbytes(123))
+# print(random.shuffle(a))
+# print(a)
+# Find the mean of:[10, 20, 30, 40, 50]
+# a=[10,20,30,40,50]
+# print(random.mea)
+
+
+# QFind the standard deviation of:[10, 12, 23, 23, 16, 23, 21, 16]
+a=[10, 12, 23, 23, 16, 23, 21, 16]
+print(statistics.stdev(a))
