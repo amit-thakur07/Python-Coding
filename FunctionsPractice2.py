baa59a3f30cb6ba649f75066b090972c21f4ab02
Amit Thakur <amitthakur70072@gmail.com>
2026-03-16 09:41:14 +0530

    Add files via upload

---
+++ FunctionsPractice2.py
@@ -0,0 +1,10 @@
+# Write a function to check whether a number is prime
+# def prime(number):
+#     return(number%1==0 and number%number==0)
+# number=int(input("enter a number: "))
+# print("it's a prime number")
+# prime(number)
+
+n=5
+for i in range(1,11):
+    print(f"{n} X {11-i}={n*i}")
