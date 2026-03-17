30e99fa1414c1df05ae6b2123fe109ae876a1845
Amit Thakur <amitthakur70072@gmail.com>
2026-03-17 10:11:21 +0530

    Add files via upload

---
+++ funQ.py
@@ -0,0 +1,29 @@
+#first def function : 
+#  def func1():
+#     print("hello world of functions")
+# func1()
+
+# write a progrsm to average a user 
+# def avg():
+#     a=int(input("enter your number: "))
+#     b=int(input("enter second number"))
+#     c=int(input("enter third number: "))
+#     average=(a+b+c)/3
+#     print(average)...........................function defination
+# avg().............................function call
+# print("thank you next")
+# avg()....2nd function call
+# print("ok next")
+# avg()...........3rd function call
+
+# greet user with good day using a function'
+# def greet():
+#     i=input("enter your name: ")
+#     print(i)
+
+# functions with some argument single  and multiple parameter----argument
+def goodDay(name,ending):
+    print("hii!"+name)
+    print(ending)
+a=goodDay("amit thakur","ok by")
+print(a)
