60c7a2fd6cefd0ff1296fe2cc44e6bc683f77ead
Amit Thakur <amitthakur70072@gmail.com>
2026-03-18 10:17:56 +0530

    Add files via upload

---
+++ staticmethod.py
@@ -0,0 +1,54 @@
+# class new:
+#     @staticmethod
+#     def n(a,b):
+#         return a+b
+# print(new.n(2,5))
+
+# class Student:
+#     old_school="asc"
+#     @classmethod
+#     def new(cls,new_school):
+#         cls.old_school=new_school
+        
+
+# class calculator:
+#     @staticmethod
+#     def sum(a,b):
+#         return a+b
+# print(calculator.sum(2,3))
+
+
+
+# class Math_utils:
+#     @staticmethod
+#     def two():
+#      num=int(input("entr number: "))
+#      square=num**2
+#      cube=num**3
+#      return square,cube
+# print(Math_utils.two())
+
+
+# class geometry:
+#     @staticmethod
+#     def area():
+#         r=int(input(" "))
+#         area_of_circle=(22/7)*r*r
+#         return area_of_circle
+# print(geometry.area())
+
+
+
+# class Student:
+#     school = "ABC"
+
+#     @classmethod
+#     def change_school(cls, new_school):
+#         cls.school = new_school
+# Student.change_school("xyz school")
+# print(Student.school)
+ 
+
+class Car():
+    wheel=4
+    
