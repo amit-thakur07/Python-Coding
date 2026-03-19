30587488bbc235ca79eb79b31f5fd02349a5f499
Amit Thakur <amitthakur70072@gmail.com>
2026-03-19 09:45:38 +0530

    Add files via upload

---
+++ PracticeComprehensions.py
@@ -0,0 +1,45 @@
+ # Create a list of numbers from 1 to 20 using list comprehension.
+# new_list=[x for x in range(1,21)]
+# print(new_list)
+
+# Create a list of squares of numbers from 1 to 10
+# square=[x*x for x in range(11)]
+# print(square)
+
+# Create a list of even numbers between 1 and 50.
+# even_list=[n%2==0 for n in range(1,50)]
+# print(even_list)
+
+# Convert a list of temperatures in Celsius to Fahrenhei
+# x=[32,34,36]
+# farenhiet=[(c*9/5)+32 for c in x  ]
+# print(farenhiet)
+
+# Create a list of lengths of each word in this list
+# l1=["amit","naman","harsh","raman"]
+# new=[len(i) for i in l1]
+# print(new)
+
+# Create a list of numbers divisible by both 3 and 5 between 1 and 100.
+# divisible=[x for x in range (1,101)if x%3==0 and x%5==0]
+# print(divisible)
+
+# From a list of numbers, create a new list containing only positive numbers.
+
+# list=[-1,2,3,-4]
+# new=[i for i in list if(i>0)]
+# print(new)
+
+# From a string "HELLO WORLD", create a list of lowercase letters.
+# string_1="HELLO WORLD"
+# list1= string_1.lower()
+# x=list(list1)
+# print(x)
+# print(type(x))
+
+# Remove vowels from a string using list
+
+
+
+
+
