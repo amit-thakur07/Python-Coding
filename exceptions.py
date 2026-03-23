bf8cd500d4bcbad93c190c15b5e729a764d9fd51
Amit Thakur <amitthakur70072@gmail.com>
2026-03-23 09:51:57 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+# how a program deals with runtime errors without crashing
+# try: 
+#     x= int("1.0")
+# except ValueError:
+#  x=0
+# print(x)
+
+
+
+
+
+# a=(input("enter something: "))
+# try:
+#  int(a)
+#  print("integer")
+
+# except ValueError:
+#  try:
+#   float(a)
+#   print("float")
+#  except:
+  
+#   print("invalid")
+
+
+
+
