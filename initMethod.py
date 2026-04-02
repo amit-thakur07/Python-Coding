4f6402ec4c364d67f9a7076cf6dfd778c8bbf402
Amit Thakur <amitthakur70072@gmail.com>
2026-04-02 20:38:22 +0530

    Add files via upload

---
+++ initMethod.py
@@ -0,0 +1,46 @@
+# class Student():
+#     def __init__(self,newname):
+#         self.name= newname
+# s1=Student("rp singh")
+# print(s1.name)
+
+
+# class Car:
+#     def __init__(self,name,brand,price):
+#         self.brand=brand
+#         self.model=name
+#         self.price=price
+# s1=Car("toyota","fortuner",400000)
+# print(s1.price)
+# print(s1.brand)
+# print(s1.model)
+
+
+
+# class Student:
+#     college_name="TRAMIET"
+    
+
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+#     def welcome(self):
+#         print(f"hello student {self.name} \nage is {self.age}")
+#         print(self.college_name)
+# s1=Student("amit",21)
+# s1.welcome()        
+
+# class Student:
+#     def __init__(self,name,marks):
+#         self.name=name
+#         self.marks=marks
+#     def get_avg(self):
+#         avg=sum(self.marks)/len(self.marks)
+#         print("hi",self.name,"your avg score is:",avg)
+# s1=Student("amam",[12,23,44])
+# s2=Student("aksahr",[12,33,100])
+# s2.get_avg()
+# s1.get_avg()
+
+
