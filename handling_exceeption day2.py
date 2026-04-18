3759ec9a2cec2a9ca6f5b663620d3e2936a469b3
Amit Thakur <amitthakur70072@gmail.com>
2026-04-18 12:23:26 +0530

    Add files via upload

---
+++ handling_exceeption day2.py
@@ -0,0 +1,16 @@
+# Write a program that takes a number from the user and handles ValueError if the user enters a string
+# try:
+#  n=int(input("enter a number: "))
+#  print("you entered: ",n)
+# except ValueError:
+#  print("error:invalid input.")
+
+# Divide two numbers entered by the user and handle ZeroDivisionError.
+# try: 
+#  a=int(input("enter a number: "))
+#  b=int(input("enter another number: "))
+#  print(a/b)
+# except (ZeroDivisionError):
+#  print("base is 0 so try to give another number")
+# except (ValueError):
+#  print("you entered wrong value. enter number")
